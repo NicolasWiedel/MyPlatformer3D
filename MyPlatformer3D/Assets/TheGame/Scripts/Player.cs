7237ee5a6a712a4244833eb66dbafbb9b0790dd7
NicolasWiedel <nicolaswiedel@googlemail.com>
2023-05-06 19:04:51 +0200

    3.20. Drehen des Spielers beim Richtungswechsel

--- MyPlatformer3D/Assets/TheGame/Scripts/Player.cs
+++ MyPlatformer3D/Assets/TheGame/Scripts/Player.cs
@@ -12,10 +12,42 @@ public class Player : MonoBehaviour
     /// </summary>
     public float speed = 0.05f;
 
+    /// <summary>
+    /// Verweis auf das graphische Model.
+    /// Dient u.A. der Drehung der Laufrichtung.
+    /// </summary>
+    public GameObject model;
+
+    /// <summary>
+    /// Der Winkel zu dem sich die Figur um die eigene Achse
+    /// (=Y) drehen soll
+    /// </summary>
+    private float towardsY = 0f;
+
     // Update is called once per frame
     private void Update()
     {
-        transform.position += Input.GetAxis("Horizontal")   
-             * speed * transform.forward;
+        float h = Input.GetAxis("Horizontal");
+
+        transform.position += h * speed * transform.forward;
+
+        if(h > 0)
+        {
+            towardsY = 0f;
+        }
+        
+        else if(h < 0)
+        {
+            towardsY = -180f;
+        }
+
+        /// <summary>
+        /// Die Lerp-Methode sorgt für eine sanfte und
+        /// flüssige Drehung der Spielfigur
+        /// </summary>
+        model.transform.rotation = Quaternion.Lerp(
+            model.transform.rotation,
+            Quaternion.Euler(0f, towardsY, 0f),
+            Time.deltaTime * 10f);
     }
 }
